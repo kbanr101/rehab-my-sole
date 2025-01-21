ea169e38279521063afbc1f4fcb5db19ad829cee
Suraj-devs <surajsh64@gmail.com>
2025-01-21 23:42:13 +0530

    Update head

--- resources/views/admin/layout/head.blade.php
+++ resources/views/admin/layout/head.blade.php
@@ -31,13 +31,21 @@
     <!-- jsvectormap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/css/jsvectormap.min.css"
         integrity="sha256-+uGLJmmTKOqBr+2E6KDYs/NRsHxSkONXFHUL0fy2O/4=" crossorigin="anonymous" />
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    {{-- <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
         tinymce.init({
             selector: '#mytextarea', // ID of the textarea
             plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
             toolbar_mode: 'floating',
         });
+    </script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.9.1/summernote-lite.min.js"></script>
+    <script>
+        $(document).ready(function() {
+        $('#mytextarea').summernote({
+            tabsize: 1,
+            height: 200
+        });
+    });
     </script>
-
 </head>
